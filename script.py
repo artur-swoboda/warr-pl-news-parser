291f744aabdf2f2f4eb4a7187b648bc232cdef3a
yav <artur.swoboda@gmail.com>
2018-06-02 02:34:26 +0200

    save items to scv

--- script.py
+++ script.py
@@ -2,8 +2,11 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
-import re
+import re, codecs, csv, htmlmin
 
+with codecs.open('posts.csv', 'w', 'utf-8') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['ID', 'post_title', 'post_content', 'post_category', 'post_date', 'post_date_gmt'])
 
 def simple_get(url):
     try:
@@ -39,14 +42,14 @@ def cleanhtml(raw_html):
 get links to pages
 """
 
-start_numba = 688
+# start_numba = 8
+start_numba = 696
 links = []
 for x in range(start_numba, 0, -8):
     start = x
     end = start - 8
 
-    raw_html = simple_get(
-        'http://www.warr.pl/2005/index.php?mode=news&pg_[go]={0}&pg_[go]={1}'.format(start, end))
+    raw_html = simple_get('http://www.warr.pl/2005/index.php?mode=news&pg_[go]={0}&pg_[go]={1}'.format(start, end))
 
     html = BeautifulSoup(raw_html, 'html.parser')
 
@@ -57,13 +60,6 @@ for x in range(start_numba, 0, -8):
 
     links = links + list(reversed(temp_links))
 
-    # for i, link in enumerate(html.select('a')):
-    #     if link.text == 'Zobacz więcej':
-    #         print(i, link['href'])
-
-# links = list(reversed(links))
-print(links)
-
 """
 get page and parse it
 """
@@ -71,27 +67,58 @@ get page and parse it
 default_url = 'http://www.warr.pl/2005/'
 page_1 = 'http://www.warr.pl/2005/index.php?mode=pnews&s=915'
 
-page_html = BeautifulSoup(simple_get(page_1), 'html.parser')
+print (len(links))
 
-"""
-page elements
-"""
+def parse_page(url):
 
-# get title and pub date
-page_title_block = page_html.select('td.td_tresc span.n_naglowek > b')
-page_title = cleanhtml(str(page_title_block[0]).split('<br/>')[0])
-page_data = cleanhtml(str(page_title_block[0]).split('<br/>')[1])
+	page_html = BeautifulSoup(simple_get(url), 'html.parser')
 
-# remove .naglowek
-for tag in page_html.select('span.n_naglowek'):
-    tag.decompose()
+	"""
+	page elements
+	"""
 
-for tag in page_html():
-    for attribute in ["style", ';', 'new', 'roman', 'times']:
-        del tag[attribute]
+	# get title and pub date
+	page_title_block = page_html.select('td.td_tresc span.n_naglowek > b')
+	page_title = cleanhtml(str(page_title_block[0]).split('<br/>')[0])
+	page_data = cleanhtml(str(page_title_block[0]).split('<br/>')[1])
 
-page_content = page_html.select('td.td_tresc')[0]
+	# remove .naglowek
+	for tag in page_html.select('span.n_naglowek'):
+	    tag.decompose()
 
-# print(page_title)
-# print(page_data)
-# print(page_content)
+	for tag in page_html():
+	    for attribute in ["style", ';', 'new', 'roman', 'times']:
+	        del tag[attribute]
+
+	page_content = page_html.select('td.td_tresc')[0]
+
+	# print(page_title)
+	# print(page_data)
+	# print(page_content)
+
+	return [page_title, page_content, page_data]
+
+for x in range(len(links)):
+
+	url = default_url + links[x]
+	ID = 5000 + x
+	data = parse_page(url)
+
+	content = data[1]
+
+	# content = htmlmin.minify(str(content), remove_empty_space=True)
+
+	content = re.sub('<\/*td.*?>', '', str(content)) # remove TD
+	content = re.sub('<\/*tr.*?>', '', str(content)) # remove TR
+	content = re.sub('<\/*table.*?>', '', str(content)) # remove TABLE
+	content = re.sub('<\/*tbody.*?>', '', str(content)) # remove TBODY
+	content = re.sub('<\/*div.*?>', '', str(content)) # remove DIV
+	content = re.sub('<\/*html.*?>', '', str(content)) # remove DIV
+	content = re.sub('<\/*body.*?>', '', str(content)) # remove DIV
+
+	content = content.rstrip()
+	content = content.strip()
+
+	with codecs.open('posts.csv', 'a', 'utf-8') as csvfile:
+		post = csv.writer(csvfile, delimiter=',')
+		post.writerow([ID, data[0], content, 'Aktualności', data[2], data[2]])
